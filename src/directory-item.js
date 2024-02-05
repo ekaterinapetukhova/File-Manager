c78e41af2bb2182d4c94fbeb4a31de33f9013b8b
Ekaterina Petukhova <katepthv12@gmail.com>
2024-02-05 21:26:37 +0100

    feat: implement fucntion which list files in current directory

---
+++ src/directory-item.js
@@ -0,0 +1,6 @@
+export class DirectoryItem {
+    constructor(name, type) {
+      this.name = name;
+      this.type = type;
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,60 @@
 import { stdout, stdin, argv } from 'process';
-import { createWriteStream } from 'fs';
-import readline from 'readline';
-
-const rl = readline.createInterface({
-  output: stdout,
-  input: stdin,
-});
+import { createReadStream, readdir } from 'fs';
+import { createInterface } from 'readline';
+import { homedir } from 'os';
+import { join, dirname, extname } from 'path';
+import { DirectoryItem } from './directory-item.js';
 
 const username =
   argv
     .filter((item) => item.startsWith('--'))
     .join('')
     .split('=')[1] || 'Username';
+const homePath = dirname(homedir());
 
-stdout.write(`Welcome to the File Manager, ${username}!`)
+const rl = createInterface({
+  output: stdout,
+  input: stdin,
+});
+
+const readStream = createReadStream(homePath);
+let currentDirectory = join(homePath, username);
+
+stdout.write(`Welcome to the File Manager, ${username}!\n`);
 
 const closeConsole = () => {
-  stdout.write(`Thank you for using File Manager, ${username}, goodbye!`)
+  stdout.write(`Thank you for using File Manager, ${username}, goodbye!`);
   rl.close();
-}
+};
 
 rl.on('line', (data) => {
   if (data.toString() === '.exit') {
     closeConsole();
+  } else {
+    switch (data.toString()) {
+      case 'ls':
+        readDirectory(currentDirectory);
+        break;
+      default:
+        console.log('lol');
+    }
+    stdout.write(`You are currently in ${currentDirectory}\n`);
   }
 });
 
-rl.on('SIGINT', closeConsole)
+const readDirectory = (path) => {
+  const directoryItems = [];
 
+  readdir(path, { withFileTypes: true }, (err, files) => {
+    if (err) throw new Error('Error!');
 
-// stdin.on('data', (usernameData) => {
-//   stdout.write(`Welcome to the File Manager, ${usernameData ? usernameData : 'Username'}!`);
-//   username = usernameData;
-// });
+    files.forEach((file) => {
+      const item = new DirectoryItem(file.name, file.isFile() ? 'file' : 'directory');
+      directoryItems.push(item);
+    });
+
+    console.table(directoryItems);
+  });
+};
+
+rl.on('SIGINT', closeConsole);
